4c6bb5fe34395790169105c51f1e9ab62736b57e
angel <angel20170238b@gmail.com>
2025-05-13 16:30:58 -0500

    commit

--- components/AddReportScreen/AddReport.js
+++ components/AddReportScreen/AddReport.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, ScrollView, Pressable, Modal, FlatList} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, ScrollView, Pressable, Modal, FlatList, Alert, KeyboardAvoidingView, Platform, StyleSheet, TouchableOpacity, BackHandler} from 'react-native';
 import { AddReportStyles as styles } from './StyleAddReport'; 
 import AntDesign from '@expo/vector-icons/AntDesign';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Entypo from '@expo/vector-icons/Entypo';
+
+import * as Location from 'expo-location';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+
 
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
@@ -14,18 +18,36 @@ export default function AddReport() {
   const crops = [{name: 'Corn', id:'1d'}, {name:'Soybean', id:'2'}, {name:'Rice', id:'3'}];
   const fertilizers = [{name:'Urea', id:'a'}, {name:'Compost', id:'b'}, {name:'NPK', id:'c'}];
   const soils = [ {name:'Sandy', id:'s1'},{ name:'Clay', id: 's2'}, { name: 'Silty', id: 's3' }];
+  const [farmName, setFarmName] = useState('');
+  const [location, setLocation] = useState(null);
+  const [gps, setGps] = useState(null)
+  const [crop, setCrop] = useState('crop');
+  const [fertilizer,setFertilizer] = useState('fertilizer');
+  const [soil, setSoil] = useState('soil');
+  const [photo, setPhoto] = useState('');
+  const [observation, setObservation] = useState('');
+  const [callback, setSelectCallback] = useState(null);
+  const YOUR_API_KEY = '';
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loadLocation, setLoadLocation] = useState(null);
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const [showCamera, setShowCamera] = useState(false)
 
-  const toggleModal = (data, title) => {
+
+  const toggleModal = (data, title, setStateCallback) => {
     setDataModal(data);
     setModalTitle(title);
+    setSelectCallback(()=> setStateCallback)
     setIsModalVisible(!isModalVisible);
 
   };
   const Item = ({name}) => {
     return (
       <View>
-        <Pressable style={{borderColor: '#ccc', borderWidth:2, margin:2, borderRadius: 10, 
-                           alignItems:'center', padding:5
+        <Pressable style={({pressed})=>[styles.button, pressed && styles.pressButton]} onPress={()=> {
+          if (callback) callback(name);
+          setIsModalVisible(!isModalVisible)
         }}>
           <Text>{name}</Text>
         </Pressable>
@@ -33,116 +55,246 @@ export default function AddReport() {
     );
   };
   const renderItem = ({item}) => <Item name={item.name} />;
+  {/* i need to add stroe insam device without sql */}
+  const getLocation = async () => {
+    setLoadLocation('Loading...')
+
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access location was denied');
+      return;
+    }
+  
+    let gps = await Location.getCurrentPositionAsync({});
+    const latitude = gps.coords.latitude;
+    const longitude = gps.coords.longitude;
+  
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${YOUR_API_KEY}&units=metric`)
+      .then(res => res.json())
+      .then(data => {
+        setLocation({
+          main: data.weather[0].main,
+          lon: data.coord.lon,
+          lat: data.coord.lat,
+          des: data.weather[0].description,
+          temp: data.main.temp,
+          country: data.sys.country
+        });
+      })
+      .catch(err => Alert.alert("Error", "Hubo un problema al obtener los datos del clima. Intenta de nuevo más tarde."));
+      setLoadLocation(null);
+  };
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+  function takePhoto() {
+    setShowCamera(true);
+  }
+
+  useEffect(() => {
+    if (!permission || !permission.granted) {
+      requestPermission();
+    }
+  }, []);
+
 
   return (
-    <ScrollView style={styles.container}>
+    <KeyboardAvoidingView 
+          style={[styles.container]}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0} // importante
+      >
+      <ScrollView keyboardDismissMode='on-drag'>
 
-      {/* Farm name */}
-      <View>
-        <Text>Farm name</Text>
-        <TextInput 
-          style={styles.input}
-          placeholder='Farm Name'
-        />
-      </View>
-      
-      {/*Whetter and localization */}
-      <View>
-        <Text>Wetter and location</Text>
-        {/* Whetter conditions*/}
-        <View style={{flexDirection:'row'}}>
+        {/* Farm name */}
+        <View style={{paddingTop:20}}>
+          <Text>Farm name</Text>
           <TextInput 
-            style={[styles.input, {flex:4}]}
-            placeholder='Whetter conditions'
+            style={styles.input}
+            value={farmName}
+            onChangeText={setFarmName}
           />
-          <Pressable style={{flex:1, margin:10, alignItems:'center', justifyContent:'center', backgroundColor:'#ddd', borderRadius:8}}>
-            <Text style={{fontSize:13}}>🌤️ </Text>
-          </Pressable>
         </View>
-        {/* GPS coordinates */}
-        <View style={{flexDirection:'row'}}> 
-          <TextInput 
-            style={[styles.input, {flex:4}]}
-            placeholder='GPS Coordinates'
-          />
-          <Pressable style={{flex:1, margin:10, alignItems:'center', justifyContent:'center', backgroundColor:'#ddd', borderRadius:8}}>
-            <Text style={{fontSize:13}}>📍</Text>
-          </Pressable>
-        </View>
-      </View>
-
-      {/* Crop type */}
-      <View>
-        <Text>Select conditions</Text>
-        <View style={{flexDirection:'row'}}>
-          <Pressable onPress={()=>toggleModal(crops, 'Crops')} style={[styles.input, {backgroundColor:'#ddd', flex:1}]}>
-            <Text>Crop</Text>
-          </Pressable>      
-        {/* Fertilizer types */}
-          <Pressable onPress={()=>toggleModal(fertilizers, 'Fertilizers')} style={[styles.input, {backgroundColor:'#ddd', flex:1}]}>
-            <Text>Fertizer</Text>
-          </Pressable>
-        {/* Soil type */}
-          <Pressable onPress={()=>toggleModal(soils, 'Soils')} style={[styles.input, {backgroundColor:'#ddd', flex:1}]}>
-            <Text>Soil</Text>
-          </Pressable>
-        </View>
-      </View>
-
-      {/* Load or Take Photo */}
-      <View>
-        <Text>Image</Text>
-        <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
-        <Pressable style={{flex:1, alignItems:'center', justifyContent:'center', 
-                          borderColor: '#ccc', borderStyle: 'dashed',borderRadius:8, 
-                          borderWidth:2, padding:10, margin:10, marginLeft:30,
-                          height:120}}>
-          <Entypo name="camera" size={40} color="black" />
-          <Text>Take Photo</Text>
-        </Pressable>
-        <Pressable style={{flex:1, alignItems:'center', justifyContent:'center', 
-                          borderColor: '#ccc', borderStyle: 'dashed',borderRadius:8, 
-                          borderWidth:2, padding:10, margin:10, marginRight:30, 
-                          height:120}}>
-          <FontAwesome name="photo" size={40} color="black" />
-          <Text>Upload Image</Text>
-        </Pressable>
-        </View>
-      </View>
-
-      <View>
-        <Text>Observations</Text>
-        <TextInput style={[styles.input, {height:100}]} numberOfLines={4} maxLength={50}/>
-      </View>
-
-      <Modal visible={isModalVisible} animationType="slide" transparent={true}>
-          <View style={{
-            flex: 1,
-            justifyContent: 'center', 
-            alignItems: 'center',       
-            backgroundColor: 'rgba(0,0,0,0.5)'
-            }}>
-              <View style={{
-                          width:'80%',
-                          borderRadius: 20,
-                          backgroundColor: 'white',
-                          padding: 15,
-                          alignItems:'center'}}>
-                <Text>Select Option</Text>
-                <FlatList 
-                  data={dataModal}
-                  keyExtractor={item => item.id}
-                  renderItem = {renderItem}>
-                </FlatList>
-                
-                <Pressable onPress={toggleModal} style={{marginTop:20, padding:5, backgroundColor: 'red', borderRadius:20}}>
-                  <Text style={{color: 'white' }}>Close</Text>
-                </Pressable>
-              </View>
+        
+        {/*Whetter and localization */}
+        <View>
+          <Text>Wetter and location</Text>
+          {/* Whetter conditions*/}
+          <View style={{flexDirection:'row'}}>
+            <Pressable 
+                onPress={getLocation}
+                style={({pressed})=>[styles.button, {flex:3}, pressed && styles.pressButton]}>
+              <Text style={{fontSize:13}}>Get local enviroment 🌤️ 📍 </Text>
+            </Pressable>
+            <Pressable 
+                onPress={()=> setLocation('')}
+                style={({pressed})=>[styles.button, {flex:1}, pressed && styles.pressButton]}>
+              <Text style={{fontSize:13}}>remove </Text>
+            </Pressable>
           </View>
-      </Modal>
-      
+          {location ? (
+            <View style={{alignItems:'center', alignSelf:'center', backgroundColor:'rgb(134, 191, 229)', width:'50%', borderRadius:20}}>
+            <Text>Conditions: {location.main}</Text>
+            <Text>Longitud: {location.lon}</Text>
+            <Text>Latitud:  {location.lat}</Text>
+            <Text>Description: {location.des}</Text>
+            <Text>Temperature: {location.temp}</Text>
+            <Text>Country: {location.country}</Text>
+            </View>
+          ): (
+            <>
+            <Text>{loadLocation}</Text></>
+          )}
+        </View>
 
-    </ScrollView>
+        {/* Crop Fertilizer Soils */}
+        <View>
+          <Text>Conditions</Text>
+          <View style={{flexDirection:'row'}}>
+            <Pressable onPress={()=>toggleModal(crops, 'Crops', setCrop)} style={({pressed})=>[
+              styles.input, {backgroundColor:'#ddd', flex:1}, pressed && {backgroundColor:'rgb(151, 158, 165)'}
+            ]}>
+              <Text>{crop}</Text>
+            </Pressable>      
+          {/* Fertilizer types */}
+            <Pressable onPress={()=>toggleModal(fertilizers, 'Fertilzers', setFertilizer)} style={({pressed})=>[
+                styles.input, {backgroundColor:'#ddd', flex:1}, pressed && {backgroundColor:'rgb(151, 158, 165)'}
+              ]}>
+              <Text>{fertilizer}</Text>
+            </Pressable>
+          {/* Soil type */}
+            <Pressable onPress={()=>toggleModal(soils, 'Soils', setSoil)} style={({pressed})=>[
+                styles.input, {backgroundColor:'#ddd', flex:1}, pressed && {backgroundColor:'rgb(151, 158, 165)'}
+              ]}>
+              <Text>{soil}</Text>
+            </Pressable>
+          </View>
+        </View>
+
+        {/* Load or Take Photo */}
+        <View>
+          <Text>Image</Text>
+
+          {/* take photo */}
+          <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+          <Pressable style={({pressed})=>[
+                      styles.imageButton, pressed && {backgroundColor:'rgb(151, 158, 165)'}
+                    ]} onPress={takePhoto}>
+            <Entypo name="camera" size={40} color="black" />
+            <Text>Take Photo</Text>
+          </Pressable>
+
+          {/* load photo 
+          <Pressable style={({pressed})=>[
+            styles.imageButton, pressed && {backgroundColor:'rgb(151, 158, 165)'}
+          ]}>
+            <FontAwesome name="photo" size={40} color="black" />
+            <Text>Upload Image</Text>
+          </Pressable>
+          */}
+          </View>
+        </View>
+
+
+
+        {/* Observations */}
+        <View>
+          <Text>Observations</Text>
+          <TextInput 
+                  value={observation}
+                  onChangeText={setObservation}
+                  style={[styles.input, {height:100, textAlignVertical: 'top'}]} 
+                  numberOfLines={4} maxLength={50} multiline={false}/>
+        </View>
+
+        <Pressable style={({pressed})=>[styles.button, pressed && {backgroundColor:'#2150fd'}]}>
+          <Text>Submit</Text>
+        </Pressable>
+
+        <Modal visible={isModalVisible} animationType="slide" transparent={true}>
+            <View style={{
+              flex: 1,
+              justifyContent: 'center', 
+              alignItems: 'center',       
+              backgroundColor: 'rgba(0,0,0,0.5)'
+              }}>
+                <View style={{
+                            width:'80%',
+                            borderRadius: 20,
+                            backgroundColor: 'white',
+                            padding: 15,
+                            alignItems:'center'}}>
+                  <FlatList 
+                    data={dataModal}
+                    keyExtractor={item => item.id}
+                    renderItem = {renderItem}
+                    ListHeaderComponent={()=> <Text>{modalTitle}:</Text>}
+                    >
+                  </FlatList>
+                  
+                  <Pressable onPress={toggleModal} style={{marginTop:20, padding:5, backgroundColor: 'red', borderRadius:20}}>
+                    <Text style={{color: 'white' }}>Close</Text>
+                  </Pressable>
+                </View>
+            </View>
+        </Modal>
+
+        {showCamera && (
+          <Modal visible={showCamera} animationType="slide">
+            <View style={stylesCamera.container}>
+              <CameraView style={stylesCamera.camera} facing={facing}>
+                <View style={stylesCamera.buttonContainer}>
+                  <TouchableOpacity style={stylesCamera.button} onPress={toggleCameraFacing}>
+                    <Text style={stylesCamera.text}>Flip</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={stylesCamera.button} onPress={() => (
+                    Alert.alert('Photo taked')
+                  )}>
+                    <Text style={stylesCamera.text}>Take</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={stylesCamera.button} onPress={() => setShowCamera(false)}>
+                    <Text style={stylesCamera.text}>Close</Text>
+                  </TouchableOpacity>
+                </View>
+              </CameraView>
+            </View>
+          </Modal>
+        )}
+
+
+
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
+
+
+const stylesCamera = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
--- components/AddReportScreen/StyleAddReport.js
+++ components/AddReportScreen/StyleAddReport.js
@@ -12,117 +12,33 @@ export const AddReportStyles = StyleSheet.create({
     },
     container: {
       flex: 1,
-      padding: 20,
+      paddingLeft: 20,
+      paddingRight: 20,
       backgroundColor: '#fff'
     },
-
-
-
-
-
-
-    scrollContent: {
-      paddingBottom: 40
-    },
-    inputGroup: {
-      marginBottom: 20,
-      position: 'relative'
-    },
-    label: {
-      marginBottom: 8,
-      fontWeight: '500',
-      color: '#333'
-    },
-
-    inputRow: {
-      flexDirection: 'row',
-      alignItems: 'center'
-    },
-    flex1: {
-      flex: 1
-    },
-    iconButton: {
-      marginLeft: 10,
-      padding: 8
-    },
-    suggestionsContainer: {
-      position: 'absolute',
-      top: '100%',
-      left: 0,
-      right: 0,
-      backgroundColor: '#fff',
+    button: {
       borderWidth: 1,
       borderColor: '#ddd',
       borderRadius: 8,
-      marginTop: 4,
-      zIndex: 10,
-      maxHeight: 200,
-      elevation: 3
-    },
-    suggestionItem: {
       padding: 12,
-      flexDirection: 'row',
-      alignItems: 'center',
-      borderBottomWidth: 1,
-      borderBottomColor: '#eee'
-    },
-    suggestionText: {
-      marginLeft: 8,
-      fontSize: 16
-    },
-    pickerInput: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      paddingRight: 12
-    },
-    placeholder: {
-      color: '#999'
-    },
-    selectedValue: {
-      color: '#000'
-    },
-    modalOptions: {
-      backgroundColor: '#fff',
-      borderRadius: 8,
-      padding: 10
-    },
-
-    modalView: {
-      margin: 20,
-      backgroundColor: 'white',
-      borderRadius: 20,
-      padding: 35,
-      alignItems: 'center',
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-    },
-    modalOptionText: {
+      margin: 10,
       fontSize: 16,
-      padding: 12
+      alignItems:'center',
+      backgroundColor:'#ddd', 
     },
-    multilineInput: {
-      minHeight: 100,
-      textAlignVertical: 'top'
-    },
-    submitButton: {
-      backgroundColor: '#2e86c1',
-      padding: 16,
-      borderRadius: 8,
-      alignItems: 'center',
-      marginTop: 20
-    },
-    submitButtonText: {
-      color: '#fff',
-      fontWeight: '600',
-      fontSize: 16
-    },
-    errorText: {
-      color: '#e74c3c',
-      marginTop: 5,
-      fontSize: 14
-    }
+    imageButton:{
+      flex:1, alignItems:'center', justifyContent:'center', 
+      borderColor: '#ccc', borderStyle: 'dashed',borderRadius:8, 
+      borderWidth:2, padding:10, margin:10, marginLeft:30,
+      height:120},
+
+    pressButton: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 8,
+        padding: 12,
+        margin: 10,
+        fontSize: 16,
+        backgroundColor:'rgb(151, 158, 165)', 
+      }
   });
